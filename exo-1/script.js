5979be00c9eb43766f6a887260f5bd9309fab8b6
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-04-04 08:43:31 +0200

    exo-2: 1-2 - Sélection d'éléments avec jquery

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,5 +1,5 @@
 /* La différence entre JS et JQuery:
-JQuery est une librairie JS: des moreceaux de codes prêts à l'emploi, mis à disposition par d'autre développeurs, à réutiliser et modifier.
+JQuery est une librairie JS: des morceaux de codes prêts à l'emploi, mis à disposition par d'autre développeurs, à réutiliser et modifier.
 
    Ajout des scripts:
 Comme mon script JS fera appel à cette librairie, je doit placer le script jquery en amont, afin que la syntaxe jquery que j'utilisera en JS soit reconnue.
--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,29 @@
-console.log('exercice 2');
+/* Les différents sélecteurs JQuery:
 
+* sélection par la balise html : $("li")
+* sélection par la class : $(".hidden")
+* sélection par l'id : $("#title")
+* sélection par l'attribut : 
+    - si l'attribut est présent: $("[href]")
+    - si l'attribut a une valeur donnée: $("[href = 'index.html']")
+etc.
+*/
+
+$(document).ready(function (){
+
+
+    //1. Sélectionner le titre du blog : "John's Blog".
+
+    //$("[id='title']").hide();
+    $("[id='title']").addClass("test");
+    //$("h4").addClass("test");
+    //$("h4#title").addClass("test");
+
+    
+    // 2. Sélectionner tous les éléments du side-menu de gauche.
+
+    $("[class = 'col-sm-3 sidenav']").addClass("test");
+    //$(".sidenav").addClass("test");
+    }
+
+)
