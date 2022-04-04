78015dfb9d22be469c25ed19ccd09261c7ed36ab
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-04-04 09:06:43 +0200

    exo-2: 2-5 - Sélection d'éléments

--- exo-2/script.js
+++ exo-2/script.js
@@ -13,17 +13,23 @@ $(document).ready(function (){
 
 
     //1. Sélectionner le titre du blog : "John's Blog".
-
     //$("[id='title']").hide();
     $("[id='title']").addClass("test");
     //$("h4").addClass("test");
     //$("h4#title").addClass("test");
 
-    
-    // 2. Sélectionner tous les éléments du side-menu de gauche.
 
+    // 2. Sélectionner tous les éléments du side-menu de gauche.
     $("[class = 'col-sm-3 sidenav']").addClass("test");
     //$(".sidenav").addClass("test");
+
+    // 3. Sélectionner le footer
+    //$("footer").addClass("test");
+    $("footer").hide();
+
+    // 4. Sélectionner la barre de recherches
+    $(".input-group").hide();
+
     }
 
 )
